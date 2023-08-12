42e42c7f2eeba22abaa9eb30f65dd54ef08fe323
Shaun Pant <crizone.shaun@gmail.com>
2023-08-13 00:14:27 +0545

    window.onload didn't work on mobile browsers

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -3,28 +3,40 @@ import { useEffect, useState } from "react";
 
 const Preloader = () => {
   const [pageLoaded, setPageLoaded] = useState(false);
-  const handlePageLoad = () => {
-    setPageLoaded(!pageLoaded);
-  };
-  useEffect(() => {
-    // document.ready = () => {
-    //   handlePageLoad();
-    // };
-    window.onload = () => {
-      setTimeout(() => {
-        handlePageLoad();
-      }, 500);
-    };
-  }, []);
+  // const handlePageLoad = () => {
+  //   setPageLoaded(!pageLoaded);
+  // };
+  // useEffect(() => {
+  //   // document.readyState = () => {
+  //   //   handlePageLoad();
+  //   // };
+  //   window.onload = () => {
+  //     setInterval(() => {
+  //       handlePageLoad();
+  //       console.log("inside use effect");
+  //     }, 500);
+  //   };
+  // }, []);
+
+  setTimeout(() => {
+    setPageLoaded(true);
+  }, 700);
   return (
     <div>
       {/* --------this doesnot work---------- */}
       {/* <div style={pageLoaded && { display: "none" }} id="preloader"></div> */}
       {/* --------but this works---------- */}
-      {/* <div style={pageLoaded ? { display: "none" } : null} id="preloader"></div> */}
+      {/* {setInterval(() => {
+        setPageLoaded(true);
+      }, 500)} */}
+      {/* <div
+        style={pageLoaded ? { display: "none" } : null}
+        className="preloader"
+      ></div> */}
       {/* --------new--------- */}
+
       <div
-        style={true ? { opacity: "0", zIndex: "-1" } : null}
+        style={pageLoaded ? { opacity: "0", zIndex: "-1" } : null}
         className="preloader"
       ></div>
     </div>
